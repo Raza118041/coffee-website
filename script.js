ac3f34a1be36cbbf312b18396705a0156d6b2e6d
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-05-04 18:53:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const menuOpenButton = document.querySelector('#menu-open-button');
+const menuCloseButton = document.querySelector('#menu-close-button');
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+})
+
+
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.remove("show-mobile-menu");
+});
+
+const swiper = new Swiper('.testimonial-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var my_element = document.getElementById("my_element");
+
+const scrollBtn = document.querySelector('.stt');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      scrollBtn.style.display = 'block';
+    } else {
+      scrollBtn.style.display = 'none';
+    }
+  });
+
+  scrollBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
