4dbe4a58cef03ac6b385385519120f39d453c1b7
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-05-06 23:39:03 +0500

    hamburgur hide when user click link

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const menuOpenButton = document.querySelector('#menu-open-button');
 const menuCloseButton = document.querySelector('#menu-close-button');
 
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove("show-mobile-menu");
+  });
+});
+
 menuOpenButton.addEventListener("click", () => {
   document.body.classList.toggle("show-mobile-menu");
 })
